bcea02d55a91c53f30d6101cc301baad086a2f9e
unknown <aliiobeid01@gmail.com>
2022-11-13 20:57:43 +0200

    backend:create the index file with the require packages

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth.routes');
+const usersRoutes = require('./routes/users.routes');
+
+require('dotenv').config();
+require('./config/db.config')
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.listen(process.env.PORT, (err)=>{
+  if(err) throw err;
+  console.log(`server running on port ${process.env.PORT}`);
+})
