25569fc2b0b6e3592a1fb9d93d42ca3a868a18e9
unknown <aliiobeid01@gmail.com>
2022-11-14 23:18:12 +0200

    frontend:add RegisterScreen to the navigator container

--- frontend/App.js
+++ frontend/App.js
@@ -23,6 +23,7 @@ export default function App() {
           }}
         >
           <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
 
         </Stack.Navigator>
         
