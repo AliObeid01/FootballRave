cce88ebe7ac401f3beaf63c33c9e5883ef25ade9
unknown <aliiobeid01@gmail.com>
2022-11-14 22:58:43 +0200

    frontend:create the component LeagueCard

---
+++ frontend/src/components/LeagueCard.js
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import { Text,StyleSheet, Image,TouchableOpacity } from 'react-native'
+import { COLORS } from '../core/COLORS'
+import { useNavigation } from '@react-navigation/native'
+
+const LeagueCard = (props) => {
+  const navigation = useNavigation();
+  return (
+    
+   <TouchableOpacity delayPressIn={70} style={styles.card_template} onPress={() => navigation.navigate(props.screenName,{title: props.name})}>
+        <Image style={styles.card_image} source={props.path}/> 
+        <Text style={styles.card_title} >{props.name}</Text> 
+   </TouchableOpacity>
+   
+  )
+}
+
+const styles = StyleSheet.create({
+  card_template:{
+    flexDirection: 'column',
+    width: '45%',
+    margin: 5,
+    borderRadius:10,
+    borderWidth:0.5,
+    borderColor:'white',
+    backgroundColor:COLORS.primaryColor,
+  },
+  card_image: {
+    width: '100%',
+    height: 150,
+    borderRadius:10,
+    resizeMode: "cover",
+  },
+  card_title: {
+    color: COLORS.secondaryColor,
+    textAlign: 'center',
+  }
+})
+
+export default LeagueCard
