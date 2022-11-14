dc99ab8fcce6552f433d04d0fcba334ee3b35e93
unknown <aliiobeid01@gmail.com>
2022-11-14 03:17:14 +0200

    backend:add league_fixtuers route to the users routes file

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,10 +1,10 @@
 const {Router} = require('express');
-const {getLeagues,getLeagueTeams} = require('../controllers/user.controller');
+const {getLeagues,getLeagueTeams,getLeagueFixtures} = require('../controllers/user.controller');
 const authUserMiddleware = require('../middlewares/user.middleware');
 const router = Router();
 
 router.get('/leagues', authUserMiddleware, getLeagues);
 router.post('/teams', authUserMiddleware, getLeagueTeams);
-
+router.post('/league_fixtures', authUserMiddleware, getLeagueFixtures);
 
 module.exports = router;
