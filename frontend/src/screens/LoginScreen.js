293e687e7d1fc4ea2cac17b1a0cea3327941b470
unknown <aliiobeid01@gmail.com>
2022-11-17 01:28:17 +0200

    frontend:add Title component to display axios error message

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -5,6 +5,7 @@ import Background from '../components/Background'
 import Logo from '../components/Logo'
 import Button from '../components/Button'
 import TextInput from '../components/TextInput'
+import Title from '../components/Title'
 import { COLORS } from '../core/COLORS'
 import { emailValidator } from '../helpers/emailValidator'
 import { passwordValidator } from '../helpers/passwordValidator'
@@ -57,7 +58,7 @@ export default function LoginScreen({ navigation }) {
 
   return (   
     <Background>
-      
+      <Title>{authError}</Title>
       <Logo />
       <TextInput
         label="Email"
