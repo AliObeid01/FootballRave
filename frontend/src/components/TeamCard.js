5e172a8423804dfb5bdd7c41021332a0bac38097
unknown <aliiobeid01@gmail.com>
2022-11-17 16:57:40 +0200

    frontend:fix image display in Teams screen

--- frontend/src/components/TeamCard.js
+++ frontend/src/components/TeamCard.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 150,
     borderRadius:10,
-    resizeMode: "cover",
+    resizeMode: "contain",
   },
   card_title: {
     color: COLORS.secondaryColor,
