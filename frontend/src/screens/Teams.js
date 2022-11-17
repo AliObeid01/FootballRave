0d9374998b903f2e759f812e8f095621529f4bf6
unknown <aliiobeid01@gmail.com>
2022-11-17 16:52:38 +0200

    linking:link getleagueTeams api to Teams screen

--- frontend/src/screens/Teams.js
+++ frontend/src/screens/Teams.js
@@ -1,46 +1,45 @@
 import * as React from 'react'
+import { useEffect, useState } from "react"
 import { ScrollView,View } from 'react-native'
 import { COLORS } from '../core/COLORS'
 import TeamCard from '../components/TeamCard'
+import axios from "axios"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 export default function Teams({route}) {
 
-  //const [teams, setTeams] = useState([]);
+  const [teams, setTeams] = useState([]);
   const league_id = route.params.league_id;
-  console.log(league_id)
-  // useEffect(() => {
-  //   const getTeams= async ()=>{
-  //     const token = await AsyncStorage.getItem('@token')
-  //     axios({
-  //       method: "POST",
-  //       data,
-  //       url: `http://192.168.1.50:5000/user/teams`,
-  //       headers:{
-  //         "Authorization" : "Bearer " +token
+  const data={
+    league_id   
+  }
   
-  //        }
-  //     }).then((res) => {
-  //       setTeams(res.data.data); 
-  //     });
-  //   }
-  //   getTeams();
-  // }, []);
+  useEffect(() => {
+    const getTeams= async ()=>{
+      const token = await AsyncStorage.getItem('@token')
+      axios({
+        method: "POST",
+        data,
+        url: `http://192.168.1.50:5000/user/teams`,
+        headers:{
+          "Authorization" : "Bearer " +token
+  
+         }
+      }).then((res) => {
+        setTeams(res.data.data);
+        console.log(teams)
+      });
+    }
+    getTeams();
+  }, []);
 
 return (
     <ScrollView style={{backgroundColor:COLORS.InputColor,height:'100%'}}>
     <View style={{flexDirection: 'row',justifyContent: 'center',alignItems: 'center',flexWrap: 'wrap'}}>
-        <TeamCard name='Man UTD' path={require('../assets/man.png')} screenName='Team'/>
-        <TeamCard name='Man City' path={require('../assets/city.jpg')} screenName='Team'/>
-        <TeamCard name='Arsenal' path={require('../assets/arsenal.jpg')} screenName='Team'/>
-        <TeamCard name='Man UTD' path={require('../assets/man.png')} screenName='Team'/>
-        <TeamCard name='Man City' path={require('../assets/city.jpg')} screenName='Team'/>
-        <TeamCard name='Arsenal' path={require('../assets/arsenal.jpg')} screenName='Team'/>
-        <TeamCard name='Man UTD' path={require('../assets/man.png')} screenName='Team'/>
-        <TeamCard name='Man City' path={require('../assets/city.jpg')} screenName='Team'/>
-        <TeamCard name='Arsenal' path={require('../assets/arsenal.jpg')} screenName='Team'/>
-        <TeamCard name='Man UTD' path={require('../assets/man.png')} screenName='Team'/>
-        <TeamCard name='Man City' path={require('../assets/city.jpg')} screenName='Team'/>
-        <TeamCard name='Arsenal' path={require('../assets/arsenal.jpg')} screenName='Team'/>
+        {teams.map((team) => {
+               return <TeamCard name={team.name} path={{uri:team.logo}} screenName='Team'/>
+            })}  
+        
     </View>
     </ScrollView>
   )
