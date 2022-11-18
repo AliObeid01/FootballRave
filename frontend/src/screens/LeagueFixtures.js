d7f72c48e6a08e40dcd9f61e391544a83e36cd3f
unknown <aliiobeid01@gmail.com>
2022-11-18 15:17:47 +0200

    frontend:add condition on the match status in leagueFixtures screen

--- frontend/src/screens/LeagueFixtures.js
+++ frontend/src/screens/LeagueFixtures.js
@@ -36,6 +36,7 @@ useEffect(() => {
 return (  
     <ScrollView style={{backgroundColor:COLORS.InputColor}}>
       {fixtures.map((fix) => {
+        //console.log(new Date(fix.date.substr(0,10))<new Date('2022-11-01'))
         if(fix.status=='FT'){
         return (
         <View style={{marginTop: 10,marginLeft:10,marginRight:10,marginBottom:10,borderRadius: 10,backgroundColor: COLORS.primaryColor,}}>
@@ -44,12 +45,29 @@ return (
         </View>
         )
       }
+      else if(fix.status=='NS'&& new Date(fix.date.substr(0,10))>new Date('2022-11-01')){
         return (
         <View style={{marginTop: 10,marginLeft:10,marginRight:10,marginBottom:10,borderRadius: 10,backgroundColor: COLORS.primaryColor,}}>
         <FixtureCard name={new Date(fix.date.substr(0,10)).toDateString()}/>    
         <MatchCard time={fix.date.substr(11,5)+' '+fix.timezone} screenName='MatchDetails' team1={fix.teams.home.name} team2={fix.teams.away.name} team1Avatar={{uri:fix.teams.home.logo}} team2Avatar={{uri:fix.teams.away.logo}}/>
         </View>
         )
+      }
+      else if(fix.status=='TBD'){
+        return (
+        <View style={{marginTop: 10,marginLeft:10,marginRight:10,marginBottom:10,borderRadius: 10,backgroundColor: COLORS.primaryColor,}}>
+        <FixtureCard name={new Date(fix.date.substr(0,10)).toDateString()+' '+'(Time to be Determined)'}/>    
+        <MatchCard time={fix.date.substr(11,5)+' '+fix.timezone} screenName='MatchDetails' team1={fix.teams.home.name} team2={fix.teams.away.name} team1Avatar={{uri:fix.teams.home.logo}} team2Avatar={{uri:fix.teams.away.logo}}/>
+        </View>
+        )
+      }
+      return (
+        <View style={{marginTop: 10,marginLeft:10,marginRight:10,marginBottom:10,borderRadius: 10,backgroundColor: COLORS.primaryColor,}}>
+        <FixtureCard name={new Date(fix.date.substr(0,10)).toDateString()+' '+'(Postpone)'}/>    
+        <MatchCard time={fix.date.substr(11,5)+' '+fix.timezone} screenName='MatchDetails' team1={fix.teams.home.name} team2={fix.teams.away.name} team1Avatar={{uri:fix.teams.home.logo}} team2Avatar={{uri:fix.teams.away.logo}}/>
+        </View>
+        )
+
     })}       
     </ScrollView>    
   )
