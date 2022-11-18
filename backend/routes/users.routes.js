42ba89995ed0b8c7aa06382e01ea9a2e50bb721c
unknown <aliiobeid01@gmail.com>
2022-11-19 01:47:59 +0200

    backend:add TopAssist route to the userroutes

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const {getLeagues,getLeagueTeams,getLeagueFixtures,getLeagueStandings,getLeagueTopScores} = require('../controllers/user.controller');
+const {getLeagues,getLeagueTeams,getLeagueFixtures,getLeagueStandings,getLeagueTopScores,getLeagueTopAssists} = require('../controllers/user.controller');
 const authUserMiddleware = require('../middlewares/user.middleware');
 const router = Router();
 
@@ -8,5 +8,6 @@ router.post('/teams', authUserMiddleware, getLeagueTeams);
 router.post('/league_fixtures', authUserMiddleware, getLeagueFixtures);
 router.post('/league_standings', authUserMiddleware, getLeagueStandings);
 router.post('/TopScores', authUserMiddleware, getLeagueTopScores);
+router.post('/TopAssists', authUserMiddleware, getLeagueTopAssists);
 
 module.exports = router;
