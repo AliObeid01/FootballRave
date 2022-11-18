a97ca7da45fda6006745d655a51d5ec28641caa3
unknown <aliiobeid01@gmail.com>
2022-11-18 15:27:14 +0200

    frontend:create TopScores screen

---
+++ frontend/src/screens/TopScores.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View} from 'react-native';
+import { Table, TableWrapper, Row, Rows, Col } from 'react-native-table-component';
+import { COLORS } from '../core/COLORS'
+import axios from "axios"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { useEffect, useState } from "react"
+
+export default function BasicTable({route}) {
+     
+    return (
+    <View></View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
