e47c860e4956092d90306efa8afc0c25532c0acd
unknown <aliiobeid01@gmail.com>
2022-11-18 03:14:04 +0200

    backend:add getleagueStandings function to the user controller

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,10 +1,11 @@
 const {Router} = require('express');
-const {getLeagues,getLeagueTeams,getLeagueFixtures} = require('../controllers/user.controller');
+const {getLeagues,getLeagueTeams,getLeagueFixtures,getLeagueStandings} = require('../controllers/user.controller');
 const authUserMiddleware = require('../middlewares/user.middleware');
 const router = Router();
 
 router.get('/leagues', authUserMiddleware, getLeagues);
 router.post('/teams', authUserMiddleware, getLeagueTeams);
 router.post('/league_fixtures', authUserMiddleware, getLeagueFixtures);
+router.post('/league_standings', authUserMiddleware, getLeagueStandings);
 
 module.exports = router;
