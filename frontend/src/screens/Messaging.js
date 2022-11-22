934b9931221332e93a495fd94a8c948c17213b9a
unknown <aliiobeid01@gmail.com>
2022-11-22 03:22:07 +0200

    frontend:create and design Messaging screen

---
+++ frontend/src/screens/Messaging.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { View, TextInput, Text, FlatList, Pressable } from "react-native";
+import socket from "../utils/socket";
+import MessageComponent from "../component/MessageComponent";
+import { styles } from "../utils/styles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const Messaging = ({ route, navigation }) => {
+	const [user, setUser] = useState("");
+
+
+	return (
+		<View style={styles.messagingscreen}>
+			<View
+				style={[
+					styles.messagingscreen,
+					{ paddingVertical: 15, paddingHorizontal: 10 },
+				]}
+			>
+				{chatMessages[0] ? (
+					<FlatList
+						data={chatMessages}
+						renderItem={({ item }) => (
+							<MessageComponent item={item} user={user} />
+						)}
+						keyExtractor={(item) => item.id}
+					/>
+				) : (
+					""
+				)}
+			</View>
+
+			<View style={styles.messaginginputContainer}>
+				<TextInput
+					style={styles.messaginginput}
+					onChangeText={(value) => setMessage(value)}
+				/>
+				<Pressable
+					style={styles.messagingbuttonContainer}
+					onPress={handleNewMessage}
+				>
+					<View>
+						<Text style={{ color: "#f2f0f1", fontSize: 20 }}>SEND</Text>
+					</View>
+				</Pressable>
+			</View>
+		</View>
+	);
+};
+
+export default Messaging;
